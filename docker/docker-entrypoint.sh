e602ea9d1652f497fa19015dc0f49832a36c2d9b
Sergey Ivanov <ivanovknitting@gmail.com>
2023-07-04 17:22:59 +0300

    Fixdocker-entrypoint.sh

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -2,7 +2,6 @@
 #
 set -e
 
-apk add sudo
 sed -i -e "/background-color:/s/#ffffff;/${REDIS_COMMANDER_BG_COLOR};/" /redis-commander/web/static/css/default.css
 
 exec sudo --preserve-env --user=redis /redis-commander/docker/entrypoint.sh
